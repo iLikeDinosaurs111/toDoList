9bc8f0890f9058f05abd64af217a156e0be22afb
iLikeDinosaurs111 <arjun.bhumula@gmail.com>
2025-06-23 20:17:48 -0400

    almost fully completed dictionary

--- src/toDoList.py
+++ src/toDoList.py
@@ -1,5 +1,5 @@
 import os
-
+from datetime import datetime
 
 def clear_terminal():
     """Clears the terminal screen."""
@@ -26,8 +26,12 @@ def current_tasks():
     if len(tasks) == 0:
         print("no added tasks")
         return
-    for i in range(len(tasks)):
-        print(str(i+1) + ". " + tasks[i])
+    task_keys = list(tasks.keys())
+    for i in range(len(task_keys)):
+        due_date = tasks[task_keys[i]]
+        print(str(i+1) + ". " + task_keys[i] + " due " + str(due_date.strftime("%B")) + " " + str(due_date.strftime("%-d") + ", " + str(due_date.strftime("%Y"))) + " at " + str(due_date.strftime("%-H")) + ":" + str(due_date.strftime("%M")))
+    
+    # print(tasks)
 
 def print_result(result_text):
     clear_terminal()
@@ -35,27 +39,32 @@ def print_result(result_text):
         print(result_text)
     current_tasks()
 
-def save_list_to_file(name_of_list):
-
+def save_list_to_file():
+    # return
     with open("tasks.txt", "w") as file:
-        for item in tasks:
-            if item != "": 
-                file.write(item + "~")
+        tasks_keys = list(tasks.keys())
+        for key, value in tasks.items():
+            file.write(key + "~" + str(value.strftime(format_data)) + "\n")
 
 
-
+format_data = "%d/%m/%y %H:%M"
 
 file = open("tasks.txt", "a")
 read_file = open("tasks.txt")
 
-tasks = []
-build = ""
-for character in read_file.read():
-    if character != "~":
-        build += character
-    else:
-        tasks.append(build)
-        build = ""
+tasks = {}
+
+for item in read_file.readlines():
+    parts = item.split("~")
+    tasks[parts[0]] = datetime.strptime(parts[1].strip(), format_data)
+
+# build = ""
+# for character in read_file.read():
+#     if character != "~":
+#         build += character
+#     else:
+#         tasks.append(build)
+#         build = ""
 
 
 
@@ -69,10 +78,14 @@ while True:
         print("name of task you want to add")
         new_task = input()
 
+        print("date when the task is due: [DD/MM/YY HH:MM]")
+        input_time = input()
 
+        task_due_date = datetime.strptime(input_time, format_data)
 
-        tasks.append(new_task)
-        save_list_to_file(tasks)
+        tasks[new_task] = task_due_date
+
+        save_list_to_file()
         print_result("task sucessfully added")
 
 
@@ -81,8 +94,13 @@ while True:
         print("number of the task you want to remove")
         current_tasks()
         to_remove = input()
-        del tasks[int(to_remove)-1]
-        save_list_to_file(tasks)
+
+        tasks_keys = list(tasks.keys())
+
+        del tasks[tasks_keys[int(to_remove)-1]]
+
+
+        save_list_to_file()
         print_result("task sucessfully removed")
 
 
@@ -91,11 +109,16 @@ while True:
         print("number of the task you want to replace")
         current_tasks()
         replacement_index = input()
-        clear_terminal()
-        print("name of which you want to use to replace")
-        replacement = input()
-        tasks[int(replacement_index)-1] = replacement
-        save_list_to_file(tasks)
+        # clear_terminal()
+        print("date when the task is due: [DD/MM/YY HH:MM]")
+        replacement_time = input()
+
+        task_due_date = datetime.strptime(replacement_time, format_data)
+
+        tasks_keys = list(tasks.keys())
+
+        tasks[tasks_keys[int(replacement_index)-1]] = task_due_date
+        save_list_to_file()
         print_result("task sucessfully replaced")
 
     
@@ -104,9 +127,9 @@ while True:
 
 
     if choice == "reset":
-        tasks = []
+        tasks = {}
         print_result("task sucessfully reset")
-        save_list_to_file(tasks)
+        save_list_to_file()
         
 
     if choice == "exit":
